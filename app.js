6906c768b312167df64eadad8159fa50e674ed55
MarkGarOliver <marcosgarcia.oliveira02@gmail.com>
2022-03-02 08:39:43 -0300

    upload arqs do projeto estruturado

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*<label class="todo__item">
+<input type="checkbox">
+<div>teste de item 1</div>
+<input type="button" value="X">
+</label>*/
+
+const criarItem = () => {
+    const item = document.createElement('label')
+    item.classList.add('todo__item')
+    item.innerHTML = `
+        <input type="checkbox">
+            <div>teste de item 1</div>
+        <input type="button" value="X">
+
+    `  
+    document.getElementById('todolist').appendChild(item)
+}
+
+criarItem()
