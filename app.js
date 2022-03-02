7b5fe01eab7ba0deeba3df707fbc1dc3f419ed6f
MarkGarOliver <marcosgarcia.oliveira02@gmail.com>
2022-03-02 10:10:43 -0300

    projeto funcional finalizado

--- app.js
+++ app.js
@@ -1,21 +1,87 @@
 'use strict';
 
-/*<label class="todo__item">
-<input type="checkbox">
-<div>teste de item 1</div>
-<input type="button" value="X">
-</label>*/
+//comentar código..
 
-const criarItem = () => {
+const getBanco = () => JSON.parse(localStorage.getItem('todolist')) ?? [] 
+const setBanco = (banco) => localStorage.setItem('todolist', JSON.stringify(banco))
+
+const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label')
     item.classList.add('todo__item')
     item.innerHTML = `
-        <input type="checkbox">
-            <div>teste de item 1</div>
-        <input type="button" value="X">
+        <input type="checkbox" ${status} data-indice = ${indice}>
+            <div>${tarefa}</div>
+        <input type="button" value="X" data-indice = ${indice}>
 
     `  
+
     document.getElementById('todolist').appendChild(item)
+    
+
+
 }
 
-criarItem()
+let novoItem = document.getElementById('novoItem')
+
+const enter = (event) => {
+    console.log(event.key)
+    const txt = event.target.value
+    if (event.key == 'Enter'){
+        const banco = getBanco()
+        banco.push({'tarefa:': txt, 'status': ''})
+        setBanco(banco)
+
+        atualizarTela
+        ()
+        event.target.value = ''
+    }
+}
+
+
+
+const limparTarefa = () =>{
+    const todolist = document.getElementById('todolist')
+    while(todolist.firstChild){
+        todolist.removeChild(todolist.lastChild)
+    }
+}
+
+const atualizarTela = () => {
+    limparTarefa()
+    const banco = getBanco()
+    banco.forEach( (item, indice) => criarItem(item["tarefa:"], item.status, indice))
+}
+
+const removerItem = (indice) => {
+    const banco = getBanco()
+
+    banco.splice(indice, 1)
+    setBanco(banco)
+    atualizarTela
+    ()
+}
+
+const atualizarItem = (indice) => {
+    const banco = getBanco()
+    banco[indice].status = banco[indice].status == '' ? 'checked' : ''
+    setBanco(banco)
+    atualizarTela()
+}
+const clickItem = (event) => {
+    const elemento = event.target
+    console.log(elemento.dataset.indice)
+
+    if (elemento.type == 'button'){
+        const indice = elemento.dataset.indice
+        removerItem(indice)
+    } else if (elemento.type == 'checkbox'){
+        const indice = elemento.dataset.indice
+        atualizarItem(indice)
+    }
+}
+
+novoItem.addEventListener('keypress', enter)
+document.getElementById('todolist').addEventListener('click', clickItem)
+
+atualizarTela
+()
